cc811b1f5e78fcf256aec02bddd8080e3306835b
vqsto <romain.croizat@hotmail.fr>
2015-05-10 17:12:10 +0200

    skeleton

---
+++ server/publish.js
@@ -0,0 +1,9 @@
+  // Only publish tasks that are public or belong to the current user
+  Meteor.publish("tasks", function () {
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
+  });
