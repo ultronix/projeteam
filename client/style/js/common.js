278fb288951a46a3d9cb6a24ca7dde2d65af59a0
Florentmarie <fmaire@entropic-synergies.com>
2015-08-04 10:20:43 +0200

    click out filter

--- client/style/js/common.js
+++ client/style/js/common.js
@@ -1,4 +1,14 @@
 $( document ).ready(function() {
-	console.log( "ready!" );
+
+	$(document).mouseup(function (e){
+	    var container = $(".filter");
+
+	    if (!container.is(e.target) // if the target of the click isn't the container...
+	        && container.has(e.target).length === 0) // ... nor a descendant of the container
+	    {
+	       	$(".container").removeClass("filter-deployed");
+	       	$(".filter").removeClass("deploy-filter");
+	    }
+	});
 
 });
